d027dccd48b92f9b6c3771a2d041ed542a6ecd8f
mvciek <mpeciakk@gmail.com>
2021-10-03 23:29:23 +0200

    Multiboot header

--- Kernel/src/main.cpp
+++ Kernel/src/main.cpp
@@ -1,8 +1,9 @@
 #include <Hardware/GDT.hpp>
 #include <Hardware/IDT.hpp>
 #include <Terminal.hpp>
+#include <Multiboot.hpp>
 
-extern "C" [[noreturn]] void kmain(void* mbi, unsigned int multibootMagic) {
+extern "C" [[noreturn]] void kmain(multiboot_info* mbi, unsigned int multibootMagic) {
     Terminal terminal;
     terminal.clear();
 
